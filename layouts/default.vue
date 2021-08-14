0c34398d9dc0143300a45aa071ab5b0673f70058
nathakrit061103jnt <std60010912540@acc.msu.ac.th>
2021-08-14 10:20:40 +0700

    update

---
+++ layouts/default.vue
@@ -0,0 +1,158 @@
+<template>
+  <v-app dark id="inspire">
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      color="primary"
+      dark
+    >
+      <!--      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />-->
+
+      <v-toolbar-title style="width: 350px">
+        <NuxtLink to="/">
+          <span class="white--text" style="text-decoration: none"
+            ><v-icon>mdi-truck</v-icon>&nbsp;ShipIT</span
+          >
+        </NuxtLink>
+      </v-toolbar-title>
+      <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+        label="Search"
+        class="hidden-sm-and-down pl-10 ml-4"
+      />
+      <v-spacer />
+      <v-btn icon>
+        <v-icon>mdi-account-circle</v-icon>
+      </v-btn>
+      <v-btn v-on="on" icon>
+        <v-badge content="2" value="2" color="green" overlap>
+          <v-icon>mdi-bell</v-icon>
+        </v-badge>
+      </v-btn>
+      <NuxtLink to="/cart" class="v-btn">
+        <v-btn v-on="on" icon>
+          <v-badge content="2" value="2" color="green" overlap>
+            <v-icon>mdi-cart</v-icon>
+          </v-badge>
+        </v-btn>
+      </NuxtLink>
+    </v-app-bar>
+    <v-content>
+      <v-bottom-navigation :value="activeBtn" color="primary" horizontal>
+        <NuxtLink to="/" class="v-btn">
+          <v-btn>
+            <span>Home</span>
+          </v-btn>
+        </NuxtLink>
+
+        <v-menu open-on-hover offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on">
+              <span>Shop</span>
+            </v-btn>
+          </template>
+          <v-card class="mx-auto" max-width="344" outlined>
+            <v-list-item v-for="(item, i) in items" :key="i" @click="">
+              <v-list-item-title v-text="item.title" />
+            </v-list-item>
+          </v-card>
+        </v-menu>
+
+        <NuxtLink to="/shop" class="v-btn">
+          <v-btn>
+            <span>Shop</span>
+          </v-btn>
+        </NuxtLink>
+        <NuxtLink to="/product" class="v-btn">
+          <v-btn>
+            <span>Product</span>
+          </v-btn>
+        </NuxtLink>
+        <NuxtLink to="/blog" class="v-btn">
+          <v-btn>
+            <span>Blog</span>
+          </v-btn>
+        </NuxtLink>
+      </v-bottom-navigation>
+    </v-content>
+    <v-main style="padding: 0px">
+      <!-- <v-container> -->
+      <Nuxt />
+      <!-- </v-container> -->
+    </v-main>
+    <v-footer :padless="true">
+      <v-card flat tile width="100%" class="secondary white--text text-center">
+        <v-card-text>
+          <v-btn class="mx-4 white--text" icon>
+            <v-icon size="24px">mdi-home</v-icon>
+          </v-btn>
+          <v-btn class="mx-4 white--text" icon>
+            <v-icon size="24px">mdi-email</v-icon>
+          </v-btn>
+          <v-btn class="mx-4 white--text" icon>
+            <v-icon size="24px">mdi-calendar</v-icon>
+          </v-btn>
+          <v-btn class="mx-4 white--text" icon>
+            <v-icon size="24px">mdi-delete</v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-card-text class="white--text pt-0">
+          Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet.
+          Mauris cursus commodo interdum. Praesent ut risus eget metus luctus
+          accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim
+          a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula
+          lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus
+          iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum
+          tempor vel ut orci. Orci varius natoque penatibus et magnis dis
+          parturient montes, nascetur ridiculus mus.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>ShipIT</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      // items: [
+      //   {
+      //     icon: 'mdi-apps',
+      //     title: 'Welcome',
+      //     to: '/'
+      //   },
+      //   {
+      //     icon: 'mdi-chart-bubble',
+      //     title: 'Inspire',
+      //     to: '/inspire'
+      //   }
+      // ],
+      items: [
+        { title: 'T-Shirts', to: '/product' },
+        { title: 'Jackets', to: '/product' },
+        { title: 'Shirts', to: '/product' },
+        { title: 'Jeans', to: '/product' },
+        { title: 'Shoes', to: '/product' },
+      ],
+      activeBtn: 1,
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
